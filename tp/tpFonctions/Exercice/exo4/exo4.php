18ac90da3504d89ab20e4e60ef972c3a2fd56afe
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-08-05 20:23:07 +0200

    update exo4

--- tp/tpFonctions/Exercice/exo4/exo4.php
+++ tp/tpFonctions/Exercice/exo4/exo4.php
@@ -1,1 +1,42 @@
 <?php
+
+function calculerTrianglePascal($n) {
+    $triangle = [];
+
+    for ($i = 0; $i < $n; $i++) {
+        $ligne = [];
+
+        for ($j = 0; $j <= $i; $j++) {
+            if ($j === 0 || $j === $i) {
+                $ligne[$j] = 1;
+            } else {
+                $ligne[$j] = $triangle[$i - 1][$j - 1] + $triangle[$i - 1][$j];
+            }
+        }
+
+        $triangle[$i] = $ligne;
+    }
+
+    return $triangle;
+}
+
+function afficherTrianglePascal($triangle) {
+    $n = count($triangle);
+
+    // Pour navigateur, on utilise du HTML
+    echo "<pre>";  // préserve les espaces
+    foreach ($triangle as $i => $ligne) {
+        echo str_repeat(" ", ($n - $i) * 2);
+        foreach ($ligne as $valeur) {
+            echo $valeur . "   ";
+        }
+        echo "\n"; // retour à la ligne dans <pre> fonctionne
+    }
+    echo "</pre>";
+}
+
+$n = 6;
+$triangle = calculerTrianglePascal($n);
+afficherTrianglePascal($triangle);
+
+?>
