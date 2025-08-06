3cd6f261f4e70dcaee6e1e72841d52d5dee000c2
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-08-06 16:16:50 +0200

    fin de mise en forme composer

---
+++ composer/src/Entity/Voiture.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entity;
+
+class Voiture
+{
+    public function rouler(): string
+    {
+        return 'ROULER';
+    }
+
+}
--- composer/src/index.php
+++ composer/src/index.php
@@ -1,1 +1,55 @@
 <?php
+namespace App;
+
+require_once '../vendor/autoload.php';
+
+use App\Entity\Voiture as Bagnole;
+
+$voiture = new Bagnole();
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Exemple d'autoload</h1>
+<?php
+
+echo $voiture->rouler();
+
+$dateDepart = new \DateTime("2025-05-01 10:30:00");
+
+var_dump($dateDepart);
+
+//premiere pratique
+
+$dateArrive = clone $dateDepart;
+$dateArrive = $dateArrive->modify('+1 hour');
+
+var_dump($dateArrive);
+
+//meilleur pratique
+// et rajout de la timezone européenne
+
+$dateDepart = new \DateTimeImmutable("2025-05-01 10:00:00");
+$dateDepart->setTimezone(new \DateTimeZone("Europe/Paris"));
+$dateArrive = $dateDepart->modify('+1 hour');
+$dateArrive->setTimezone(new \DateTimeZone("Europe/Paris"));
+
+
+?>
+
+<p>Date de départ : <?= $dateDepart->format('d/m/Y h:i:s'); ?> </p>
+<p>Date d'arrivé : <?= $dateArrive->format('d/m/Y h:i:s'); ?> </p>
+
+
+</body>
+</html>
