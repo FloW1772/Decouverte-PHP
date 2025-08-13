68786b4d1b0b57c12380e9cd2a3880fe43ffdc6a
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-08-13 19:53:24 +0200

    fin tp

--- Symfony/devS/bucket-list4/src/Controller/WishController.php
+++ Symfony/devS/bucket-list4/src/Controller/WishController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -8,16 +9,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/wishes', name: 'app_wish_list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishes = $wishRepository->findBy(
+            ['isPublished' => true],
+            ['dateCreated' => 'DESC']
+        );
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
     }
 
     #[Route('/wish/{id}', name: 'app_wish_detail')]
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
+        $wish = $wishRepository->find($id);
+
+        if (!$wish) {
+            throw $this->createNotFoundException('Idée non trouvée');
+        }
+
         return $this->render('wish/detail.html.twig', [
-            'id' => $id
+            'wish' => $wish
         ]);
     }
 }
