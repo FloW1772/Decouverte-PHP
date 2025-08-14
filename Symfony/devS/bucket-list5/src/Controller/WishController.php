7337d59ff0cea4a125a480ecc88085a6416b7226
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-08-14 17:56:11 +0200

    rajout création fichier et correction

---
+++ Symfony/devS/bucket-list5/src/Controller/WishController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use App\Repository\WishRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WishController extends AbstractController
+{
+    #[Route('/wishes', name: 'app_wish_list')]
+    public function list(WishRepository $wishRepository): Response
+    {
+        $wishes = $wishRepository->findBy(
+            ['isPublished' => true],
+            ['dateCreated' => 'DESC']
+        );
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
+    }
+
+    #[Route('/wish/{id}', name: 'app_wish_detail')]
+    public function detail(int $id, WishRepository $wishRepository): Response
+    {
+        $wish = $wishRepository->find($id);
+
+        if (!$wish) {
+            throw $this->createNotFoundException('Idée non trouvée');
+        }
+
+        return $this->render('wish/detail.html.twig', [
+            'wish' => $wish
+        ]);
+    }
+}
